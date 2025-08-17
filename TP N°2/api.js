ebdbf3824584393e6e167ede884eb11b888be0a3
isabellamernes <isabellamernes5@gmail.com>
2025-08-17 18:38:33 -0300

    .

---
+++ TP N°2/api.js
@@ -0,0 +1,8 @@
+async function obtenerProductos(limit = 0) {
+    const url = `https://fakestoreapi.com/products?limit=${limit}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+obtenerProductos(5)
